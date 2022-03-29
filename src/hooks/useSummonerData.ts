bffa491ed9a45496038ca3998b07f75c90ce6145
kmjnnhyk <rlawlsgur620@gmail.com>
2022-03-29 16:16:35 +0900

    axios와 useQuery 반환 타입 정의 해결

--- src/hooks/useSummonerData.ts
+++ src/hooks/useSummonerData.ts
@@ -1,37 +1,22 @@
 import { useQuery, UseQueryResult } from 'react-query';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { SummonerDataType } from 'types/query';
 
-const RIOT_KEY = 'RGAPI-6763462b-eaa7-42c8-b3ad-0197e2ac6283';
+const RIOT_KEY = 'RGAPI-4045b2a0-ad01-4f2d-8ecf-22fea4efdf34';
 
-async function getData(
-  nickname: string | undefined,
-): Promise<SummonerDataType | undefined> {
-  if (typeof nickname === 'string') {
-    const firstRes = await axios.get(
-      `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${nickname}?api_key=${RIOT_KEY}`,
-    );
-    const userId = firstRes.data.id;
+const getData = async (nickname: string): Promise<AxiosResponse<SummonerDataType>> => {
+  const firstRes = await axios.get(`https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${nickname}?api_key=${RIOT_KEY}`);
+  const userId = firstRes.data.id;
 
-    const secondRes = await axios.get(
-      `https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/${userId}?api_key=${RIOT_KEY}`,
-    );
-    const summonerData = secondRes.data;
+  const secondRes = await axios.get(`https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/${userId}?api_key=${RIOT_KEY}`);
 
-    return summonerData;
-  }
-  return undefined;
-}
+  return secondRes;
+};
 
-const useSummonerData = async (
-  nickname: string | null,
-): Promise<UseQueryResult<SummonerDataType, Error> | undefined> => {
-  if (typeof nickname === 'string') {
-    return useQuery(['data', nickname], await getData(nickname), {
-      enabled: false,
-    });
-  }
-  return undefined;
+const useSummonerData = (nickname: string): UseQueryResult<AxiosResponse<SummonerDataType>> => {
+  return useQuery(['data', nickname], () => getData(nickname), {
+    enabled: false,
+  });
 };
 
 export default useSummonerData;
