f40becbbbe57bc91afe82096f5d13bfe0377567a
kmjnnhyk <rlawlsgur620@gmail.com>
2022-04-05 17:33:19 +0900

    type 체크 추가

--- src/hooks/useSummonerData.ts
+++ src/hooks/useSummonerData.ts
@@ -1,8 +1,8 @@
-import { QueryKey, useQuery, UseQueryOptions, UseQueryResult } from 'react-query';
-import axios, { AxiosError, AxiosResponse } from 'axios';
+import { QueryKey, useQuery, UseQueryOptions } from 'react-query';
+import axios, { AxiosError } from 'axios';
 import { SummonerDataType } from 'types/query';
 
-const RIOT_KEY = 'RGAPI-fd0e57ea-7a56-4a0a-b38d-01bdc2fc415b';
+const RIOT_KEY = 'RGAPI-4952e881-03b9-414c-bca9-49e5045255dc';
 
 const getData = async (nickname: string): Promise<SummonerDataType> => {
   const firstRes = await axios.get(`https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${nickname}?api_key=${RIOT_KEY}`);
@@ -13,7 +13,18 @@ const getData = async (nickname: string): Promise<SummonerDataType> => {
   return secondRes.data[0];
 };
 
-const useSummonerData = <TData = SummonerDataType>(nickname: string, options?: UseQueryOptions<SummonerDataType, AxiosError, TData>) =>
-  useQuery(['data', nickname] as QueryKey, () => getData(nickname), options);
+const useSummonerData = <TData = SummonerDataType>(
+  nickname: string | null,
+  options?: UseQueryOptions<SummonerDataType, AxiosError, TData>,
+) => {
+  if (typeof nickname === 'string') return useQuery(['data', nickname] as QueryKey, () => getData(nickname), options);
+  else return null;
+};
+
+// export const useFetchOrder = <T>(
+//   orderNo: string,
+//   options?: UseQueryOptions<ServerResponse<FetchOrderResponse>, AxiosError<ErrorResponse>, T>,
+// ): UseQueryResult<T, AxiosError<ErrorResponse>> =>
+//   useQuery('fetchOrder', (): Promise<ServerResponse<FetchOrderResponse>> => fetchOrder(orderNo), options);
 
 export default useSummonerData;
