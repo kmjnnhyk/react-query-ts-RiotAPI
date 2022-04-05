c2607f72f84f3c2a8090e94f804a27ea01ebd853
kmjnnhyk <rlawlsgur620@gmail.com>
2022-04-05 17:34:24 +0900

    params만 다루는 컴포넌트로 수정하여 명확하게 기능 분리

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,15 @@
 import InfoContainer from 'Containers/InfoContainer';
 import SearchContainer from 'Containers/SearchContainer';
 import React from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 const Home = () => {
+  const [searchParams] = useSearchParams();
+  const pageParam = searchParams.get('summoner');
   return (
     <>
       <SearchContainer />
-      <InfoContainer />
+      <InfoContainer pageParam={pageParam} />
     </>
   );
 };
